d462354520beb8ba7122640db023c8b832eac169
bacaAlexandre <alexandre76b@aol.com>
2018-04-11 11:29:26 +0200

    ajout route pour partie annonce

--- FrameworkPHP/config/routes.php
+++ FrameworkPHP/config/routes.php
@@ -1,8 +1,19 @@
 <?php
 
 Route::get('/', 'HomeController@index');
-
 Route::get('/home/{id:[0-9]+}', 'HomeController@edit');
+
+
+
+//////// Accueil  ////////
 Route::get('/accueil', 'AccueilController@index');
+
+
+//////// Contact  ////////
 Route::get('/contact', 'ContactController@index');
 Route::post('/contact', 'ContactController@store');
+
+
+
+//////// Annonce ////////
+Route::get('/annonce','AnnonceCOntroller@index');
