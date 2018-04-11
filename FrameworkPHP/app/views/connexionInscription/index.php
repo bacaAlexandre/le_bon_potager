1a08533dad6d2751892e5d5d726cca315865b226
Nicolas Kamphaus <nicolas.kamphaus@viacesi.fr>
2018-04-11 22:29:00 +0200

    Modification formulaire inscription.

--- FrameworkPHP/app/views/connexionInscription/index.php
+++ FrameworkPHP/app/views/connexionInscription/index.php
@@ -22,35 +22,35 @@
       <fieldset>
         <legend>Inscription</legend>
         <div class="form-group">
-          <label for="email">Votre e-mail :</label>
+          <label for="email">Votre e-mail* :</label>
           <input type="email" name="email" id="email" class="form-control" required>
         </div>
         <div class="form-group">
-          <label for="password">Votre mot de passe :</label>
+          <label for="password">Votre mot de passe* :</label>
           <input type="password" name="password" id="password" class="form-control" minlength="8" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d-+_]{8,}$" required>
         </div>
         <div class="form-group">
-          <label for="password_repeat">Confirmer votre mot de passe :</label>
+          <label for="password_repeat">Confirmer votre mot de passe* :</label>
           <input type="password" name="password_repeat" id="password_repeat" class="form-control" minlength="8" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d-+_]{8,}$" required>
         </div>
         <div class="form-group">
-          <label for="pseudo">Votre pseudonyme :</label>
+          <label for="pseudo">Votre pseudonyme* :</label>
           <input type="text" name="pseudo" id="pseudo" class="form-control" minlength="3" maxlength="30" required>
         </div>
         <div class="form-group">
-          <label for="dep">Votre département :</label>
+          <label for="dep">Votre département *:</label>
           <select name="dep" id="dep" class="form-control" required>
             <option value="..">Choix</option>
           </select>
         </div>
         <div class="form-group">
-          <label for="city">Votre ville :</label>
+          <label for="city">Votre ville* :</label>
           <select name="city" id="city" class="form-control" required>
             <option value="..">Choix</option>
           </select>
         </div>
         <div class="form-group">
-          <label for="postalcode">Votre code postal :</label>
+          <label for="postalcode">Votre code postal* :</label>
           <select name="postalcode" id="postalcode" class="form-control" required>
             <option value="..">Choix</option>
           </select>
@@ -60,7 +60,7 @@
           <textarea rows="3" name="address" id="address" class="form-control" required></textarea>
         </div>
         <div class="form-group">
-          <label for="biography">Votre département :</label>
+          <label for="biography">Votre département* :</label>
           <select name="choix" id="serveur" class="form-control">
             <option value="Serveur1">1er Serveur</option>
             <option value="Serveur2">2eme Serveur</option>
@@ -80,10 +80,10 @@
         </div>
         <div class="form-check">
           <input type="checkbox" id="tel_visible" class="form-check-input" checked>&nbsp;
-          <label for="tel_visible" class="form-check-label">J'autorise l'affichage de mon n° de tél. ans mes annonces.</label>
+          <label for="tel_visible" class="form-check-label">J'autorise l'affichage de mon n° de tél. dans mes annonces.</label>
         </div>
         <p>* Champ obligatoire</p>
-        <button type="submit" class="btn btn-success">S'inscrir</button>
+        <button type="submit" class="btn btn-success">Inscription</button>
       </fieldset>
     </form>
   </div>
