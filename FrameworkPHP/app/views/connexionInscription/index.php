2df8ac1ef463aa9dc4d255094ff4f74e48920e7b
Christophe COUERBE <christophe.couerbe@viacesi.fr>
2018-04-11 17:33:26 +0200

    Mise à jour du formulaire.

--- FrameworkPHP/app/views/connexionInscription/index.php
+++ FrameworkPHP/app/views/connexionInscription/index.php
@@ -1,72 +1,91 @@
 <?php include(VIEW_PATH.'default/header.php'); ?>
 <div class="row">
-  <form action="<?php Route::get_uri('ConnexionInscriptionController@connexion') ?>" method="post">
-    <div class="col-md-6 md-6">
+  <div class="col-md-6">
+    <form action="<?php Route::get_uri('ConnexionInscriptionController@connexion') ?>" method="post">
       <fieldset>
         <legend>Connexion</legend>
         <div class="form-group">
           <label for="email_con">Votre e-mail :</label>
-          <input type="email" name="email" id="email_con" class="form-control">
+          <input type="email" name="email" id="email_con" class="form-control" required>
         </div>
         <div class="form-group">
           <label for="password_con">Votre mot de passe :</label>
-          <input type="password" name="password" id="password_con" class="form-control">
+          <input type="password" name="password" id="password_con" class="form-control" minlength="8" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d-+_]{8,}$" required>
         </div>
         <button type="submit" class="btn btn-success">Se connecter</button>
       </fieldset>
-    </div>
-  </form>
-  <form action="/verification.php" method="post">
-    <div class="col-md-6 md-6">
+    </form>
+  </div>
+  <div class="col-md-6">
+    <form action="/verification.php" method="post">
       <fieldset>
         <legend>Inscription</legend>
         <div class="form-group">
           <label for="email">Votre e-mail :</label>
-          <input type="email" name="email" id="email" class="form-control">
+          <input type="email" name="email" id="email" class="form-control" required>
         </div>
         <div class="form-group">
           <label for="password">Votre mot de passe :</label>
-          <input type="password" name="password" id="password" class="form-control">
+          <input type="password" name="password" id="password" class="form-control" minlength="8" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d-+_]{8,}$" required>
         </div>
         <div class="form-group">
           <label for="password_repeat">Confirmer votre mot de passe :</label>
-          <input type="password_repeat" name="password_repeat" id="password_repeat" class="form-control">
+          <input type="password" name="password_repeat" id="password_repeat" class="form-control" minlength="8" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d-+_]{8,}$" required>
         </div>
         <div class="form-group">
           <label for="pseudo">Votre pseudonyme :</label>
-          <input type="pseudo" name="pseudo" id="pseudo" class="form-control">
+          <input type="text" name="pseudo" id="pseudo" class="form-control" minlength="3" maxlength="30" required>
         </div>
         <div class="form-group">
-          <label for="address">Votre adresse :</label>
-          <textarea rows="5" id="address" class="form-control"></textarea>
-        </div>
-        <div class="form-group">
-          <label for="postal_code">Votre code postal :</label>
-          <input type="postal_code" name="postal_code" id="postal_code" class="form-control">
+          <label for="dep">Votre département :</label>
+          <select name="dep" id="dep" class="form-control" required>
+            <option value="..">Choix</option>
+          </select>
         </div>
         <div class="form-group">
           <label for="city">Votre ville :</label>
-          <input type="city" name="city" id="city" class="form-control">
+          <select name="city" id="city" class="form-control" required>
+            <option value="..">Choix</option>
+          </select>
         </div>
+        <div class="form-group">
+          <label for="postalcode">Votre code postal :</label>
+          <select name="postalcode" id="postalcode" class="form-control" required>
+            <option value="..">Choix</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <label for="address">Votre adresse :</label>
+          <textarea rows="3" name="address" id="address" class="form-control" required></textarea>
+        </div>
+        <div class="form-group">
+          <label for="biography">Votre département :</label>
+          <select name="choix" id="serveur" class="form-control">
+            <option value="Serveur1">1er Serveur</option>
+            <option value="Serveur2">2eme Serveur</option>
+          </select>
+        </div>
+
         <div class="form-group">
           <label for="phone">Votre n° de téléphone :</label>
           <input type="tel" name="phone" id="phone" class="form-control">
         </div>
         <div class="form-group">
           <label for="biography">Votre biographie :</label>
-          <textarea rows="5" id="biography" class="form-control"></textarea>
+          <textarea rows="5" name="biography" id="biography" class="form-control"></textarea>
         </div>
         <div class="form-check">
-          <input type="checkbox" id="copy" class="form-check-input">
-          <label for="copy" class="form-check-label">Je souhaite recevoir une copie de cet email</label>
+          <input type="checkbox" id="address_visible" class="form-check-input" checked>&nbsp;
+          <label for="address_visible" class="form-check-label">J'autorise l'affichage de mon adresse dans mes annonces.</label>
         </div>
         <div class="form-check">
-          <input type="checkbox" id="copy" class="form-check-input">
-          <label for="copy" class="form-check-label">Je souhaite recevoir une copie de cet email</label>
+          <input type="checkbox" id="tel_visible" class="form-check-input" checked>&nbsp;
+          <label for="tel_visible" class="form-check-label">J'autorise l'affichage de mon n° de tél. ans mes annonces.</label>
         </div>
+        <p>* Champ obligatoire</p>
         <button type="submit" class="btn btn-success">S'inscrir</button>
       </fieldset>
-    </div>
-  </form>
+    </form>
+  </div>
 </div>
 <?php include(VIEW_PATH.'default/footer.php'); ?>
