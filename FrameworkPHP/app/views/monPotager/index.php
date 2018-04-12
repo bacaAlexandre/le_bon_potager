da5ecb777a1657f9dc7ee8557728b7d74ce1dc09
Nicolas Kamphaus <nicolas.kamphaus@viacesi.fr>
2018-04-12 10:41:17 +0200

    Création de la rubrique "Mon potager"

---
+++ FrameworkPHP/app/views/monPotager/index.php
@@ -0,0 +1,17 @@
+<?php include(VIEW_PATH . 'default/header.php'); ?>
+
+    <h2>Déposez votre annonce</h2>
+
+    <form action="" method="post">
+        <label for="category"> Je recherche : </label>
+        <select name="" id="category">
+
+        </select>
+        <label for="product"> Produit :</label>
+        <select name="" id="product">
+        </select>
+    </form>
+    <button type="submit" class="btn btn-success">Valider</button>
+    </form>
+
+<?php include(VIEW_PATH . 'default/footer.php'); ?>
