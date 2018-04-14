d85f090514909199eb2819f06f6e9bc93e232002
Christophe COUERBE <christophe.couerbe@viacesi.fr>
2018-04-14 17:43:08 +0200

    Mise en forme des produits avec le formulaire de contact.

--- FrameworkPHP/app/views/contact/index.php
+++ FrameworkPHP/app/views/contact/index.php
@@ -1,22 +1,27 @@
 <?php include(VIEW_PATH . 'default/nav.php'); ?>
-<!--tu pourra supprimé les commentaires chris-->
-<!-- Bloc du produit -->
-<div>
-    <img class="img-fluid" src="<?php echo PUBLIC_URI . "img/" . $data->proImg ?>" alt="">
-    <p><?php echo $data->proNom ?></p>
-    <p><?php echo $data->puCommentaire ?></p>
-    <p>Quantité: <?php echo $data->puQuantite . " " . $data->uniLibelle ?></p>
+<div class="">
+    <h1>Vous allez être mis en relation avec le vendeur</h1>
 </div>
-<!-- Bloc du producteur -->
-<div>
-    <p><?php echo $data->utiPseudo ?></p>
-    <p><?php echo $data->utiDescription ?></p>
-    <?php if ($data->utiAdresseAffiche == 1) { ?>
-        <p><?php echo $data->utiAdresse ?></p>
-    <?php } ?>
-    <?php if ($data->utilTelAffiche == 1) { ?>
-        <p><?php echo $data->utiTel ?></p>
-    <?php } ?>
+<div class="container rounded">
+    <div class="row">
+        <div class="col-md-2">
+            <img class="img-fluid" src="<?php echo PUBLIC_URI . "img/" . $data->proImg ?>" width="150px" height="150px" alt="Tomate">
+        </div>
+        <div class="col-md-3">
+            <div class="col-md-12">
+                Vendeur : <?php echo $data->utiPseudo ?>
+            </div>
+            <div class="col-md-12">
+                Produit : <?php echo $data->proNom ?>
+            </div>
+            <div class="col-md-12">
+                Quantité : <?php echo $data->puQuantite . " " . $data->uniLibelle ?>
+            </div>
+        </div>
+        <div class="col-md-7 justify-content-md-center">
+            <?php echo $data->puCommentaire ?>
+        </div>
+    </div>
 </div>
 <div class="row">
     <div class="col-md-6 md-6">
@@ -27,31 +32,55 @@
                     <label for="nickname">Votre pseudo :</label>
                     <input type="text" name="nickname" id="nickname" class="form-control" value="<?php echo $pseudo ?>">
                 </div>
-                <div class=" form-group">
+                <div class="form-group">
                     <label for="email">Votre e-mail * :</label>
                     <input type="email" name="email" id="email" class="form-control" value="<?php echo $email ?>"
                            required>
                 </div>
-                <div class=" form-group">
+                <div class="form-group">
                     <label for="phone">Votre téléphone :</label>
                     <input type="tel" name="phone" id="phone" class="form-control" value="<?php echo $tel ?>">
                 </div>
-                <div class=" form-group">
+                <div class="form-group">
                     <label for="message">Votre message :</label>
                     <textarea rows="5" id="message" class="form-control" required></textarea>
                 </div>
-                <div class=" form-group">
-                    <!--                    TODO  j'ai rajouter un champs quantité dans le formulaire il etait mal placé sur la maquette -->
-                    <label for="message">Quantité :</label>
-                    <input type="text"><span><?php echo $data->uniLibelle ?></span>
+                <div class="form-group">
+                    <label for="quantity">Quantité :</label>
+                    <input type="number" id="quantity"><span><?php echo $data->uniLibelle ?></span>
                 </div>
                 <div class="form-check">
                     <input type="checkbox" id="copy" class="form-check-input">
                     <label for="copy" class="form-check-label">Je souhaite recevoir une copie de cet email</label>
                 </div>
-                <button type="submit" class="btn btn-success">Envoyer l'email</button>
+                <div>
+                    <button type="submit" class="btn btn-success">Envoyer l'email</button>
+                </div>
             </fieldset>
         </form>
+    </div>
+    <div class="col-md-6 md-6">
+        <h4>Informations de l'annonceur</h4>
+        <div class="form-group">
+            <div class="spe">Biographie :</div>
+            <div class="form-control"><?php echo $data->utiDescription ?></div>
+        </div>
+        <div class="form-group">
+            <div class="spe">Adresse :</div>
+            <div class="form-control">
+                <?php if ($data->utiAdresseAffiche == 1) { ?>
+                    <?php echo $data->utiAdresse ?>
+                <?php } ?>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="spe">Téléphone :</div>
+            <div class="form-control">
+                <?php if ($data->utilTelAffiche == 1) { ?>
+                    <?php echo $data->utiTel ?>
+                <?php } ?>
+            </div>
+        </div>
     </div>
 </div>
 <?php include(VIEW_PATH . 'default/footer.php'); ?>
