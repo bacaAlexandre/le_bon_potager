33c65d8105aa7d066f066093c0612a126ce5b60d
bacaAlexandre <alexandre76b@aol.com>
2018-04-14 10:55:03 +0200

    maj page contacte recupération de donnée

--- FrameworkPHP/app/views/contact/index.php
+++ FrameworkPHP/app/views/contact/index.php
@@ -1,4 +1,23 @@
 <?php include(VIEW_PATH . 'default/nav.php'); ?>
+<!--tu pourra supprimé les commentaires chris-->
+<!-- Bloc du produit -->
+<div>
+    <img class="img-fluid" src="<?php echo PUBLIC_URI . "img/" . $data->proImg ?>" alt="">
+    <p><?php echo $data->proNom ?></p>
+    <p><?php echo $data->puCommentaire ?></p>
+    <p>Quantité: <?php echo $data->puQuantite . " " . $data->uniLibelle ?></p>
+</div>
+<!-- Bloc du producteur -->
+<div>
+    <p><?php echo $data->utiPseudo ?></p>
+    <p><?php echo $data->utiDescription ?></p>
+    <?php if ($data->utiAdresseAffiche == 1) { ?>
+        <p><?php echo $data->utiAdresse ?></p>
+    <?php } ?>
+    <?php if ($data->utilTelAffiche == 1) { ?>
+        <p><?php echo $data->utiTel ?></p>
+    <?php } ?>
+</div>
 <div class="row">
     <div class="col-md-6 md-6">
         <form action="" method="post">
@@ -20,6 +39,11 @@
                 <div class=" form-group">
                     <label for="message">Votre message :</label>
                     <textarea rows="5" id="message" class="form-control" required></textarea>
+                </div>
+                <div class=" form-group">
+                    <!--                    TODO  j'ai rajouter un champs quantité dans le formulaire il etait mal placé sur la maquette -->
+                    <label for="message">Quantité :</label>
+                    <input type="text"><span><?php echo $data->uniLibelle ?></span>
                 </div>
                 <div class="form-check">
                     <input type="checkbox" id="copy" class="form-check-input">
