0ec0a0117f3a2ba4f90e40fc37a3c597320f19a6
bacaAlexandre <alexandre76b@aol.com>
2018-05-23 14:32:32 +0200

    ajout de chartjs et momentjs

--- FrameworkPHP/app/views/default/header.php
+++ FrameworkPHP/app/views/default/header.php
@@ -24,6 +24,8 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"
             integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
             crossorigin="anonymous"></script>
+    <script type="text/javascript" src="<?php echo ASSET_URL; ?>lib/momentjs/js/momentjs.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.js"></script>
     <script type="text/javascript">const PUBLIC_URL = '<?php echo PUBLIC_URL?>';</script>
     <script type="text/javascript" src="<?php echo ASSET_URL; ?>lib/jquery-ui/js/jquery-ui.js"></script>
     <script type="text/javascript" src="<?php echo ASSET_URL; ?>js/master.js"></script>
