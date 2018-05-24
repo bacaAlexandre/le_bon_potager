2b4ed1d8091f38b8b950ef0b59e4a2fa2e7b9bfb
Lucien Brouillaud <lucien.brouillaud@wanadoo.fr>
2018-05-24 16:58:27 +0200

    Refactoring du script de l'inscription

--- FrameworkPHP/public/asset/js/inscription.js
+++ FrameworkPHP/public/asset/js/inscription.js
@@ -27,55 +27,17 @@ $(function() {
         let latitude = 0;
         let longitude = 0;
 
-        if ((adresse.val().length > 0) && ($(code_postal).find(':selected').data('code-postal') !== undefined)) {
-            $.ajax({
-                url: 'https://api-adresse.data.gouv.fr/search/?q=' + adresse.val() + '&postcode=' + $(code_postal).find(':selected').data('code-postal') + '&autocomplete=0&limit=1',
-                method: 'GET',
-                success: function (data) {
-                    if (data.features.length > 0) {
-                        let feature = data.features[0];
-                        longitude = feature.geometry.coordinates[0];
-                        latitude = feature.geometry.coordinates[1];
-                    }
+        $.ajax({
+            url: 'https://api-adresse.data.gouv.fr/search/?q=' + (adresse.val().length > 0 ? adresse.length : undefined) + '&postcode=' + $(code_postal).find(':selected').data('code-postal') + '&autocomplete=0&limit=1',
+            method: 'GET',
+            success: function (data) {
+                if (data.features.length > 0) {
+                    let feature = data.features[0];
+                    longitude = feature.geometry.coordinates[0];
+                    latitude = feature.geometry.coordinates[1];
                 }
-            }).done(function() {
-                $.ajax({
-                    url: get_url('connexion/register'),
-                    type: 'POST',
-                    data: {
-                        'email': email.val(),
-                        'password': password.val(),
-                        'password_repeat': password_repeat.val(),
-                        'pseudo': pseudo.val(),
-                        'adresse': adresse.val(),
-                        'code_postal': code_postal.val(),
-                        'tel': tel.val(),
-                        'biographie': biographie.val(),
-                        'adresse_visible': (adresse_visible.val() === 'on' ? 1 : 0),
-                        'tel_visible': (tel_visible.val() === 'on' ? 1 : 0),
-                        'latitude': latitude,
-                        'longitude': longitude,
-                    }
-                }).done(function (data) {
-                    $(password).val('');
-                    $(password_repeat).val('');
-
-                    data = JSON.parse(data);
-                    if (data.length > 0) {
-                        let i;
-                        for (i = 0; i < data.length; i++) {
-                            $('#' + data[i].input).addClass('is-invalid');
-                            $(error).append('<li>' + data[i].message + '</li>');
-                        }
-                        $(error).slideDown();
-                    } else {
-                        $(error).slideUp();
-                        window.location.href = get_url('#connexion');
-                        window.location.reload(true);
-                    }
-                });
-            });
-        } else {
+            }
+        }).done(function() {
             $.ajax({
                 url: get_url('connexion/register'),
                 type: 'POST',
@@ -92,24 +54,25 @@ $(function() {
                     'tel_visible': (tel_visible.val() === 'on' ? 1 : 0),
                     'latitude': latitude,
                     'longitude': longitude,
-                },
-                success: function(data) {
-                    data = JSON.parse(data);
-                    if (data.length > 0) {
-                        let i;
-                        for (i = 0; i < data.length; i++) {
-                            $('#' + data[i].input).addClass('is-invalid');
-                            $(error).append('<li>' + data[i].message + '</li>');
-                        }
-                        $(error).slideDown();
-                    } else {
-                        $(error).slideUp();
-                    }
                 }
-            }).done(function() {
+            }).done(function (data) {
                 $(password).val('');
                 $(password_repeat).val('');
+
+                data = JSON.parse(data);
+                if (data.length > 0) {
+                    let i;
+                    for (i = 0; i < data.length; i++) {
+                        $('#' + data[i].input).addClass('is-invalid');
+                        $(error).append('<li>' + data[i].message + '</li>');
+                    }
+                    $(error).slideDown();
+                } else {
+                    $(error).slideUp();
+                    window.location.href = get_url('#connexion');
+                    window.location.reload(true);
+                }
             });
-        }
+        });
     });
 });
